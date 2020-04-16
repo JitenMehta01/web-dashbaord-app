f289f3101c6b539a5842b93638c4796e48720fbb
Jiten Mehta <jitenmehta03@gmail.com>
2020-04-16 12:14:32 +0100

    Dashboard and alert section now have been added. Issue regarding the event listener on alert.

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 // VARIABLES
-const alertSection = document.getElementById('alert-container');
+const alertContainer = document.getElementById('alert-container');
 
 
 
 
 
-// ALERT BANNER
-// alert.addEventListener('click', (e) =>{
-//   const alertX = e.target;
-//   if (alertX.className === 'alert-banner-close'){
-//     alertSection.style.display = 'none';
-//   }
-// });
+
+alertContainer.addEventListener('click', (e) =>{
+  const alertX = e.target;
+  if (alertX.id === 'alertclosesvg'){
+    alertContainer.style.display = 'none';
+  }
+});
 
 
 
@@ -21,11 +21,16 @@ const countWords = (str) => {
   return text.split(/\s+/);
 };
 
-const alertString = document.querySelector('#alert-container > p .alertmessage');
+const alertString = document.querySelector('#alert-container > .alert-inside-container > p .alertmessage');
 const alertWords = countWords(alertString);
-const mq = window.matchMedia( "(max-width: 798px)" );
+const mqDesktop = window.matchMedia( "(min-width: 798px)" );
+const mqMobile = window.matchMedia( "(max-width: 797px)" );
 
-if (alertWords.length > 10 && mq.matches){
-alertWords.splice(10);
-alertString.innerHTML = alertWords.join(' ') + ' ... ';
+if (alertWords.length > 30 && mqDesktop.matches){
+  alertWords.splice(30);
+  alertString.innerHTML = alertWords.join(' ') + ' ... ';
+}
+else if (alertWords.length > 10 && mqMobile.matches) {
+  alertWords.splice(10);
+  alertString.innerHTML = alertWords.join(' ') + ' ... '
 }
