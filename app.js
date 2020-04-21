eda497b25e007374abefde8f0337c587897a848f
Jiten Mehta <jitenmehta03@gmail.com>
2020-04-21 16:44:41 +0100

    Alert functionality completed and implemented.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const main = document.getElementById('main');
 
 const alertContainer = document.getElementById('alert-container');
-const closeContainer = document.querySelector('#alertclosesvg');
+const checkContainer = document.getElementById('alertchecksvg');
 const bellSvg = document.querySelector('.bell img');
 
 const header = document.querySelector('header');
@@ -11,44 +11,109 @@ let chartWidget = document.getElementById('#traffic-chart');
 
 
 const alertString = document.querySelector('#alert-container > .alert-inside-container > p .alertmessage');
-const mqDesktop = window.matchMedia( "(min-width: 798px)" );
-const mqMobile = window.matchMedia( "(max-width: 797px)" );
+
+// MQ
+const mqMobile = window.matchMedia("(max-width:475px)");
+const mqTablet = window.matchMedia("(min-width: 780px)");
+const mqDesktop = window.matchMedia("(min-width: 980px)");
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 // CLICK EVENT FOR THE ALERT MESSAGE.
-closeContainer.addEventListener('click', (e) =>{
-  alertContainer.style.opacity = '0';
+
+
+checkContainer.addEventListener('click', (e) =>{
+  const alertDiv = alertContainer.children[0];
+  alertDiv.style.opacity = '0';
   window.setTimeout( () => {
-    alertContainer.style.display = 'none';} ,
+    alertDiv.style.display = 'none';} ,
     1000
   );
+  if(alertDiv.style.opacity === '0'){
+    const p = document.createElement('p');
+    p.className = 'checkedalertText';
+    p.style.opacity = '0';
+    p.textContent = 'You alert message has been cleared. Click the bell icon to view it again.';
+    p.style.transition = 'all 1s ease-out';
+    window.setTimeout( () => {
+      alertContainer.style.justifyContent = 'center';
+      alertContainer.style.alignItems = 'center';
+      alertContainer.appendChild(p);
+      p.style.opacity = '1';
+      } ,
+      1000
+    );
+  }
 });
+
+
+
+// When the check element is clicked, the message stating the alert message is down below will be removed
+checkContainer.addEventListener('click', () =>{
+  if (bellSvg.parentNode.lastElementChild.tagName === 'P'){
+    const pContainer = bellSvg.parentNode;
+    const p = bellSvg.parentNode.lastElementChild;
+    p.style.opacity = '0';
+    window.setTimeout( () => {
+    pContainer.removeChild(p);
+    },1000
+    );
+  }
+
+});
+
+
+
+
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 // CLICK EVENT FOR BELL ICON. THIS WILL TRIGGER THE ALERT MESSAGE.
 bellSvg.addEventListener('click', (e) =>{
 
-  if (alertContainer.style.display === 'none'){
-  alertContainer.style.display = 'flex';
-    window.setTimeout( () => {
-    alertContainer.style.opacity = '1';} ,
-    200
-    );
-  }
 
-else{
-  const svgDiv = bellSvg.parentNode;
-  const p = document.createElement('p')
-  if(svgDiv.lastElementChild !== 'p'){
-    svgDiv.appendChild(p);
+
+      if (alertContainer.children[0].style.display === 'none' && alertContainer.children[0].style.opacity === '0' ){
+        alertContainer.removeChild(alertContainer.children[1]);
+        alertContainer.children[0].style.display = 'flex';
+        alertContainer.children[0].style.opacity = '1';
+        alertContainer.style.justifyContent = 'initial';
+        alertContainer.style.alignItems = 'initial';
+
+      }
+
+      if (alertContainer.style.display === 'none'){
+      alertContainer.style.display = 'flex';
+        window.setTimeout( () => {
+        alertContainer.style.opacity = '1';} ,
+        200
+        );
+      }
+
+    else{
+        if (mqDesktop.matches){
+          const svgDiv = bellSvg.parentNode;
+          if(svgDiv.lastElementChild.tagName !== 'P'){
+            const p = document.createElement('p')
+            svgDiv.appendChild(p);
+            p.className = 'alert-bell-text';
+            p.innerHTML = 'Your alert message is displaying down below. Click the check button to clear it of your list. ';
+            const text = document.querySelector('.alert-bell-text');
+            text.style.display = 'block';
+            text.style.right = '300px';
+            text.style.opacity = '1';
+            text.style.color = 'white';
+        }
+      }
     }
-  }
-
 });
 
+
+
+
+
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////
-// FUNCTION WHICH ADDS A MAX NO. OF WORDS OF 30 FOR ALERT BANNER ON DESKTOP, AND A MAX OF 10 ON MOBILE
+// FUNCTION WHICH ADDS A MAX NO. OF WORDS OF 30 FOR ALERT BANNER ON DESKTOP,
 const countWords = (str) => {
   const text = str.textContent;
   return text.split(/\s+/);
@@ -56,13 +121,24 @@ const countWords = (str) => {
 
 
 alertWords = countWords(alertString);
-if (alertWords.length > 30 && mqDesktop.matches){
+
+// Tblet
+if (mqTablet.matches && mqDesktop.matches === false ){
+alertWords.splice(13);
+alertString.innerHTML = alertWords.join(' ') + ' ... ';
+}
+// Desktop
+else if (mqTablet.matches && mqDesktop.matches){
   alertWords.splice(30);
   alertString.innerHTML = alertWords.join(' ') + ' ... ';
 }
-else if (alertWords.length > 10 && mqMobile.matches) {
-  alertWords.splice(10);
-  alertString.innerHTML = alertWords.join(' ') + ' ... '
+
+// Mobile
+else {
+  alertWords.splice(8);
+  alertString.innerHTML = alertWords.join(' ') + ' ... ';
+  alertString.previousElementSibling.innerHTML = '<strong>Alert</strong>';
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
